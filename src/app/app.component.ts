843817a5a0a5471686c1fe15941fd7956d2d3a1c
shubham1121 <shubhamdevrani1810@gmail.com>
2021-03-27 01:08:36 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-
+import {FormControl,FormGroup,Validators} from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,7 +7,20 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-  isVisibleUser=false;
+  userDetails = new FormGroup({
+    userName: new FormControl(),
+    userEmail: new FormControl('',Validators.required),
+    userPhone: new FormControl('',[Validators.required, Validators.pattern("[0-9 ]{10}")]),
+    userAddress: new FormControl(),
+  }) 
+  get email(){return this.userDetails.get('userEmail');}
+  get phone(){return this.userDetails.get('userPhone');}
+  collectData()
+  {
+    
+  }
+  
+  visibleUser=1;
   inputNameUpdate='';
   updateName(val)
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+
 })
 export class AppModule { }
